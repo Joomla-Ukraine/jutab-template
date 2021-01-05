f81f4532f25065f7bfa5ef1f584e42701e73fe37
Denys <denys@joomla-ua.org>
2021-01-05 02:37:49 +0200

    Add core template files

---
+++ pkg_script.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * JU Tab
+ *
+ * @package          Joomla.Site
+ * @subpackage       JU Tab
+ *
+ * @author           Denys Nosov, denys@joomla-ua.org
+ * @copyright        2017-2021 (C) Joomla! Ukraine, http://joomla-ua.org. All rights reserved.
+ * @license          GNU General Public License version 2 or later; see _LICENSE.php
+ */
+
+defined('_JEXEC') or die;
+
+class pkg_ju_tabInstallerScript extends JCckInstallerScriptApp
+{
+	protected $template_placeholder = 'ju_tab';
+}
